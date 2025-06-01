89bf978cf66b85f5886cd383309f964dddfd5711
Florian Holzmann <holzmann96flo@gmail.com>
2025-06-01 20:17:03 +0200

    removed popup page for simple action call

--- calex/src/background/background.js
+++ calex/src/background/background.js
@@ -5,3 +5,28 @@ chrome.runtime.onInstalled.addListener(({reason}) => {
       });
     }
   });
+
+
+chrome.action.onClicked.addListener(async (tab) => {
+    // if (!await statusCheck()) {
+    //     window.alert("CalGo server is not running!");
+    //     return;
+    // }
+    await requestAppLaunch(tab.url)
+});
+
+
+async function statusCheck() {
+    return await fetch('http://localhost:5252/status')
+        .then(response => {
+            return response.ok;
+        });
+}
+
+async function requestAppLaunch(url) {
+    const encodedUrl = 'http://localhost:5252/open/' + url
+        .replace('://', '~')
+        .replaceAll('/', '+');
+
+    await fetch(encodedUrl);
+}
