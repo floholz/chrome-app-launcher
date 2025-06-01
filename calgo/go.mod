4d5e2081b3bfde9d4d49c1427c67c0b31525319f
Florian Holzmann <holzmann96flo@gmail.com>
2025-06-01 18:59:42 +0200

    convert to systray app

--- calgo/go.mod
+++ calgo/go.mod
@@ -2,4 +2,22 @@ module github.com/floholz/chrome-app-launcher/calgo
 
 go 1.23.3
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/badgerodon/penv v0.0.0-20151004123538-7a4c6d64fa11
+	github.com/getlantern/systray v1.2.2
+	github.com/gorilla/mux v1.8.1
+)
+
+require (
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
--- calgo/main.go
+++ calgo/main.go
@@ -1,62 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	_ "embed"
+	"github.com/floholz/chrome-app-launcher/calgo/calgo"
+	"github.com/getlantern/systray"
 	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"os/exec"
-	"strings"
-
-	"github.com/gorilla/mux"
 )
 
+//go:embed assets/logo.ico
+var logoData []byte
+
+//go:embed assets/logout.ico
+var iconLogoutData []byte
+
 func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/open/{encodedUrl}", openHandler).Methods("GET")
-	r.HandleFunc("/create", createHandler).Methods("POST")
-
-	addr := ":5252"
-	fmt.Println("Server running on http://localhost" + addr)
-	log.Fatal(http.ListenAndServe(addr, r))
+	systray.Run(onReady, nil)
 }
 
-func openHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	encodedURL := vars["encodedUrl"]
+func onReady() {
+	systray.SetIcon(logoData)
+	systray.SetTitle("CalGo")
+	systray.SetTooltip("Chrome App Launcher - Server")
+	mQuit := systray.AddMenuItem("Quit", "Quit CalGo server")
 
-	decodedURL, err := url.PathUnescape(encodedURL)
-	if err != nil {
-		http.Error(w, "Invalid URL encoding", http.StatusBadRequest)
-		return
+	// Sets the icon of a menu item. Only available on Mac and Windows.
+	mQuit.SetTemplateIcon(iconLogoutData, iconLogoutData)
+	mQuit.SetIcon(iconLogoutData)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		err := calgo.Start(ctx)
+		if err != nil {
+			log.Printf("calgo server exited: %v", err)
+		}
+	}()
+
+	for {
+		select {
+		case <-mQuit.ClickedCh:
+			cancel()
+			systray.Quit()
+		}
 	}
-	if !strings.HasPrefix(decodedURL, "http") && !strings.HasPrefix(decodedURL, "localhost") {
-		http.Error(w, "Invalid URL. Must be 'http', 'https' or 'localhost'", http.StatusBadRequest)
-	}
-
-	decodedURL = strings.ReplaceAll(decodedURL, "~", "://")
-	decodedURL = strings.ReplaceAll(decodedURL, "+", "/")
-
-	log.Println("Received URL:", decodedURL)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Opening: %s\n", decodedURL)
-	errExec := launchChromeApp(decodedURL)
-	if errExec != nil {
-		log.Println(errExec)
-	}
-}
-
-func createHandler(w http.ResponseWriter, r *http.Request) {
-	// Implementation goes here
-	w.WriteHeader(http.StatusNotImplemented)
-	fmt.Fprintln(w, "Create handler not implemented yet")
-}
-
-func launchChromeApp(url string) error {
-	execCmd := exec.Command("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", "--app="+url)
-	execCmd.Stdout = os.Stdout
-	execCmd.Stderr = os.Stderr
-	return execCmd.Run()
 }
